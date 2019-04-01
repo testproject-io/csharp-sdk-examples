985606bc9870bf94ad8f3666510ca338d4a16b76
Marat Strelets <marat.strelets@devalore.com>
2019-04-01 12:35:25 +0300

    Added proxy DLLs and updated tests

---
+++ IOS/Test/Test/ProxyTest.cs
@@ -0,0 +1,64 @@
+using TestProject.Addons.Proxy;
+using TestProject.SDK.Common.Attributes;
+using TestProject.SDK.Common.Enums;
+using TestProject.SDK.Examples.IOS.Addon;
+using TestProject.SDK.Examples.IOS.Test.Pages;
+using TestProject.SDK.PageObjects;
+using TestProject.SDK.Tests;
+using TestProject.SDK.Tests.Helpers;
+
+namespace TestProject.SDK.Examples.IOS.Test
+{
+	[Test(Name = "Clear Fields")]
+	public class ProxyTest : IIOSTest
+	{
+		[Parameter(DefaultValue = "John Smith")]
+		public string name;
+
+		[Parameter(DefaultValue = "12345")]
+		public string password;
+
+		[Parameter(DefaultValue = "Earth")]
+		public string country;
+
+		[Parameter(DefaultValue = "Address")]
+		public string address;
+
+		[Parameter(DefaultValue = "someone@somewhere.tld")]
+		public string email;
+
+
+		public ExecutionResult Execute(IOSTestHelper helper)
+		{
+			var driver = helper.Driver;
+			var report = helper.Reporter;
+
+			driver.ResetApp();
+
+			var loginPage = PageFactory.InitElements<LoginPage>(driver);
+			report.Step("Launched TestProject Demo app", loginPage.Displayed);
+
+			loginPage.Login(name, password);
+
+			var profilePage = PageFactory.InitElements<ProfilePage>(driver);
+			report.Step($"Logged in with {name}:{password}", profilePage.Displayed);
+
+			profilePage.TypeCountry(country);
+			profilePage.TypeAddress(address);
+			profilePage.TypeEmail(email);
+
+			// Type random phone number using Addon proxy
+			var actionProxy = CiOSExampleAddon.CreateTypeRandomPhoneAction("1", 7);
+			ExecutionResult result = helper.ExecuteProxy(actionProxy, profilePage.GetPhoneElement());
+			report.Step("Type random phone number using Addon proxy", result == ExecutionResult.Passed);
+
+			// Save profile
+			profilePage.Save();
+
+			report.Step("Profile information saved", profilePage.Saved, TakeScreenshotConditionType.Always);
+
+			report.Result = "Test completed successfully";
+			return ExecutionResult.Passed;
+		}
+	}
+}
---
+++ Web/Test/TestProject.SDK.Examples.Web.Test/Test/ProxyTest.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using TestProject.SDK.Examples.Web.Test.Pages;
+using TestProject.SDK.Tests;
+using TestProject.SDK.Tests.Helpers;
+using TestProject.SDK.Common.Attributes;
+using TestProject.SDK.PageObjects;
+using TestProject.SDK.Common.Enums;
+using TestProject.SDK.Examples.Web.Addon;
+using TestProject.SDK.Reporters;
+
+namespace TestProject.SDK.Examples.Web.Test
+{
+	[Test(Name = "Test with Addon proxy")]
+	public class ProxyTest : IWebTest
+	{
+		[Parameter(DefaultValue = "John Smith")]
+		public string name;
+
+		[Parameter(DefaultValue = "12345")]
+		public string password;
+
+		[Parameter(DefaultValue = "United States")]
+		public string country;
+
+		[Parameter(DefaultValue = "Address")]
+		public string address;
+
+		[Parameter(DefaultValue = "someone@somewhere.tld")]
+		public string email;
+
+		public ExecutionResult Execute(WebTestHelper helper)
+		{
+			// Get driver initialized by TestProject Agent
+			// No need to specify browser type, it can be done later via UI
+			var driver = helper.Driver;
+			TestReporter report = helper.Reporter;
+
+			// Navigate to TestProject Demo website
+			driver.Navigate().GoToUrl("https://example.testproject.io/web/");
+
+			// Initialize the properties of the LoginPage with the driver
+			var loginPage = PageFactory.InitElements<LoginPage>(driver);
+			report.Step("Navigated to TestProject Demo", loginPage.Displayed);
+
+			// Login using provided credentials
+			loginPage.Login(name, password);
+
+			// Initialize the properties of the profilePage with the driver
+			var profilePage = PageFactory.InitElements<ProfilePage>(driver);
+			report.Step($"Logged in with {name}:{password}", profilePage.Displayed);
+
+			profilePage.SelectCountry(country);
+			profilePage.TypeAddress(address);
+			profilePage.TypeEmail(email);
+
+			// Type random phone number using Addon proxy
+			var actionProxy = new TypeRandomPhoneAction("1", 7);
+			ExecutionResult result = helper.ExecuteProxy(actionProxy, profilePage.GetPhoneElement());
+			report.Step("Type random phone number using Addon proxy", result == ExecutionResult.Passed);
+
+			// Save profile
+			profilePage.Save();
+			report.Step("Profile information saved", profilePage.Saved, TakeScreenshotConditionType.Always);
+
+			report.Result = "Test completed successfully";
+			return profilePage.Saved ? ExecutionResult.Passed : ExecutionResult.Failed;
+		}
+	}
+}
